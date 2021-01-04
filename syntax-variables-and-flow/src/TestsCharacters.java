6da6d9e7a5cb07dcb270dd0abb03331616effe0d
marianavns <mariana.vns@gmail.com>
2021-01-04 09:51:55 -0300

    feat: characters testing and values challenge

---
+++ syntax-variables-and-flow/src/TestsCharacters.java
@@ -0,0 +1,15 @@
+
+public class TestsCharacters {
+	public static void main(String[] args) {
+				
+		char letter = 'm';
+		System.out.println(letter);
+		
+		char number = 231;
+		System.out.println(number);
+		// Aqui o char vai buscar o caracter correspondente a este número na tabela unicode.
+		
+		String phrase = "Esta é a forma de escrever mais que uma letra.";
+		System.out.println(phrase);
+	}
+}
---
+++ syntax-variables-and-flow/src/ValuesChallenge.java
@@ -0,0 +1,11 @@
+
+public class ValuesChallenge {
+	public static void main(String[] args) {
+		int first = 5;
+		int second = 7;
+		first = second;
+		first = 10;
+		// Guardando "10" na variável "first", qual será o valor de "second"?
+		System.out.println(second);
+	}
+}
